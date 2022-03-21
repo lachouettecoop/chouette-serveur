de8c9e2be42e8108804719576e52b0c407109928
Vincent Bisserié <vbisserie@protonmail.com>
2022-03-21 22:11:09 +0100

    Import only used command
    Signed-off-by: Vincent Bisserié <vbisserie@protonmail.com>

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import logging
 import sys
 from logging.handlers import RotatingFileHandler
 
-from customers_populate_from_ldap import main as customers_populate_from_ldap
-from download_data_from_prod import main as download_data_from_prod
-from update_meal_voucher_products import main as update_meal_voucher_products
-from update_scales import main as update_scales
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
@@ -24,11 +19,23 @@ logger.addHandler(f_handler)
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1 or sys.argv[1] == "download_data_from_prod":
+        from download_data_from_prod import main as download_data_from_prod
+
         download_data_from_prod()
     elif sys.argv[1] == "update_meal_voucher_products":
+        from update_meal_voucher_products import (
+            main as update_meal_voucher_products,
+        )
+
         update_meal_voucher_products()
     elif sys.argv[1] == "customers_populate_from_ldap":
+        from customers_populate_from_ldap import (
+            main as customers_populate_from_ldap,
+        )
+
         customers_populate_from_ldap()
     elif sys.argv[1] == "update_scales":
+        from update_scales import main as update_scales
+
         update_scales()
     sys.exit(-1)
