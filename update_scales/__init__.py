1788ffd8f7d26371394ad5384c1c7db22fb4ce7c
Vincent Bisserie <vbisserie@gmail.com>
2022-04-05 21:22:57 +0200

    Convert LF to CRLF
    Signed-off-by: Vincent Bisserie <vbisserie@gmail.com>

--- update_scales/__init__.py
+++ update_scales/__init__.py
@@ -9,6 +9,8 @@ from helpers.odoo_api import OdooAPI
 ARTI_PREFIX = "ARTI_MAG_0000_"
 TEXT_PREFIX = "TEXT_MAG_0000_"
 SUFFIX = ".CSV"
+WINDOWS_LINE_ENDING = "\r\n"
+UNIX_LINE_ENDING = "\n"
 
 config = load_config(__name__)
 odoo_api = OdooAPI(
@@ -42,19 +44,22 @@ def main():
     if not logs:
         logging.info("Nothing to update")
         return
-    for log in logs:
-        product_text = log["product_text"]
-        product_attrs = product_text.split("#")
-        product_attrs[11] = "0"
-        log["product_text"] = "#".join(product_attrs)
     now = datetime.now()
-    date_time = now.strftime("%Y%m%d_%H%M")
+    date_time = now.strftime("%Y%m%d_%H%M%S")
     arti_file_name = f"{ARTI_PREFIX}{date_time}{SUFFIX}"
     with open(arti_file_name, "a") as f:
-        f.write("".join([e.get("product_text") for e in logs]))
+        f.write(
+            "".join([e.get("product_text") for e in logs]).replace(
+                UNIX_LINE_ENDING, WINDOWS_LINE_ENDING
+            )
+        )
     text_file_name = f"{TEXT_PREFIX}{date_time}{SUFFIX}"
     with open(text_file_name, "a") as f:
-        f.write("".join([e.get("external_text") for e in logs]))
+        f.write(
+            "".join([e.get("external_text") for e in logs]).replace(
+                UNIX_LINE_ENDING, WINDOWS_LINE_ENDING
+            )
+        )
     logging.info(f"Uploading files {arti_file_name} and {text_file_name}")
     with FTP(config.ftp.host, config.ftp.user, config.ftp.password) as ftp:
         with open(arti_file_name, "rb") as f:
