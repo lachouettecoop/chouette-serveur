22ab830a19566ac624c494fb378f45222589154a
Vincent Bisserié <vbisserie@protonmail.com>
2024-12-05 22:56:04 +0100

    Update to docker-compose v2
    Signed-off-by: Vincent Bisserié <vbisserie@protonmail.com>

--- helpers/containers.py
+++ helpers/containers.py
@@ -1,7 +1,6 @@
 import logging
-import re
 
-from compose.cli.command import get_project
+import subprocess
 
 
 class Containers:
@@ -9,61 +8,49 @@ class Containers:
         self.project = project
         self._containers = []
 
-    @property
-    def containers(self):
-        if not self._containers:
-            project = get_project(self.project)
-            self._containers = project.containers(stopped=True)
-        return self._containers
-
-    def get_container(self, service):
-        for c in self.containers:
-            if c.service == service:
-                return c
-
-    @staticmethod
-    def _exec_cmd(container, cmd):
-        logging.info('Execute "%s" in docker %s', cmd, container.name)
-        exec_id = container.create_exec(cmd)
-        result = container.start_exec(exec_id)
-        if isinstance(result, bytes):
-            result = result.decode("utf-8")
-        errors = []
-        for line in result.splitlines():
-            if line and re.search("error", line, re.IGNORECASE):
-                errors.append(line)
-        if errors:
-            return "\n".join(errors)
-        return "successful operation"
-
-    def is_running(self, service):
-        if not self.containers:
-            return False
-        if not (container := self.get_container(service)):
-            return False
-        return container.is_running
-
-    def run_cmds(self, service, cmds):
-        logging.info("Commands execution ...")
-        container = self.get_container(service)
-        for cmd in cmds:
-            result = self._exec_cmd(container, cmd)
-            if result:
-                logging.info(result)
-        logging.info("... success")
-
     def restart_all(self):
-        for c in self.containers:
-            logging.info("Restart %s", c.service)
-            c.restart()
+        try:
+            # Run `docker-compose up`
+            subprocess.run(
+                ["docker-compose", "up", "-d"],
+                cwd=self.project,
+                check=True)
+            logging.info(f"Docker Compose started successfully for {self.project}")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error running docker-compose: {e}")
+
+    def restart(self, container):
+        try:
+            # Run `docker-compose up`
+            subprocess.run(
+                ["docker-compose", "up", "-d", container],
+                cwd=self.project,
+                check=True)
+            logging.info(f"{container} started successfully for {self.project}")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error running docker-compose: {e}")
 
     def stop_all(self):
-        for c in self.containers:
-            logging.info("Stop %s", c.service)
-            c.stop()
+        try:
+            # Run `docker-compose down`
+            subprocess.run(
+                ["docker-compose", "down"],
+                cwd=self.project,
+                check=True
+            )
+            logging.info(f"Docker Compose stopped for {self.project}")
+        except subprocess.CalledProcessError as e:
+            logging.error(f"Error stopping docker-compose: {e}")
 
-    def restart(self, service):
-        for c in self.containers:
-            if c.service == service:
-                logging.info("Restart %s", c.service)
-                c.restart()
+    def run_cmds(self, containers, cmds):
+        for cmd in cmds:
+            try:
+                # Run `docker-compose down`
+                subprocess.run(
+                    ["docker-compose", "exec", containers, cmd],
+                    cwd=self.project,
+                    check=True
+                )
+                logging.info(f"Docker Compose run cmd for {self.project}")
+            except subprocess.CalledProcessError as e:
+                logging.error(f"Error run cmd docker-compose: {e}")
